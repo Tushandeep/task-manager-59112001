30b03b5c90a85018f68cf690ff8cae6b5b0f60d7
Tushandeep <tushansingh01@gmail.com>
2024-05-01 10:09:15 +0530

    Added Environment File and Implemented the Auth Page

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"1a13c7d1f40e32decdacc066a922fc95d6f554
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2647727367"
+    serviceWorkerVersion: "2259623854"
   }
 });
